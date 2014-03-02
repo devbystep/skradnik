57debc82a5ddf140157411ed23b9ac2c3b836eff
minsler <minsler.by@gmail.com>
2014-03-02 18:29:22 +0300

    Added scrolling. Added async task for translating

--- App/src/main/java/com/example/app/MainActivity.java
+++ App/src/main/java/com/example/app/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.app;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
@@ -24,6 +26,7 @@ import org.jsoup.nodes.Element;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -38,7 +41,7 @@ public class MainActivity extends ActionBarActivity {
         wordEdit = (EditText) findViewById(R.id.word_translate);
         wordView = (TextView) findViewById(R.id.view_translate);
         wordButton = (Button) findViewById(R.id.button_translate);
-
+        wordView.setMovementMethod(new ScrollingMovementMethod());
     }
 
     @Override
@@ -49,70 +52,93 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
     public void translate_click(View v) {
         String wordFromEdit = wordEdit.getText().toString();
-        if(wordFromEdit == null || wordFromEdit.isEmpty()){
+        if (wordFromEdit == null || wordFromEdit.trim().length() == 0) {
             wordView.setText("введите слово для поиска");
             return;
         }
-        String translateForWord = translate(wordFromEdit);
-        wordView.setText(Html.fromHtml(translateForWord), TextView.BufferType.SPANNABLE);
-    }
-
-    public String translate(String word) {
-        String url = "http://www.skarnik.by/search?term=" + word + "&lang=rus";
-        String result = getTranslate(url);
-        return result;
+        new TranslateTask().execute(wordFromEdit);
+        InputMethodManager imm = (InputMethodManager) getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(wordEdit.getWindowToken(), 0);
     }
 
 
-    public String getTranslate(String url) {
 
-        HttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet(url);
-        request.setHeader("User-Agent", "android");
 
-        try {
-            HttpResponse response = client.execute(request);
+    private class TranslateTask extends AsyncTask<String, String, Void> {
 
-            // Check if server response is valid
-            StatusLine status = response.getStatusLine();
-            if (status.getStatusCode() != 200) {
-                throw new IOException("Invalid response from server: " + status.toString());
+        @Override
+        protected Void doInBackground(String... strings) {
+            String wordFromEdit = Arrays.asList(strings).get(0);
+            String translateForWord = translate(wordFromEdit);
+            publishProgress(translateForWord);
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            String translateForWord = Arrays.asList(values).get(0);
+            wordView.setText(Html.fromHtml(translateForWord), TextView.BufferType.SPANNABLE);
+            wordEdit.setText("");
+            super.onProgressUpdate(values);
+        }
+
+        public String translate(String word) {
+            String url = "http://www.skarnik.by/search?term=" + word + "&lang=rus";
+            String result = getTranslation(url);
+            return result;
+        }
+
+
+        public String getTranslation(String url) {
+
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet(url);
+            request.setHeader("User-Agent", "android");
+
+            try {
+                HttpResponse response = client.execute(request);
+
+                // Check if server response is valid
+                StatusLine status = response.getStatusLine();
+                if (status.getStatusCode() != 200) {
+                    throw new IOException("Invalid response from server: " + status.toString());
+                }
+
+                // Pull content stream from response
+                HttpEntity entity = response.getEntity();
+                InputStream inputStream = entity.getContent();
+
+
+                ByteArrayOutputStream content = new ByteArrayOutputStream();
+
+                // Read response into a buffered stream
+                int readBytes = 0;
+                byte[] sBuffer = new byte[512];
+                while ((readBytes = inputStream.read(sBuffer)) != -1) {
+                    content.write(sBuffer, 0, readBytes);
+                }
+
+                String dataAsString = new String(content.toByteArray());
+
+                Document doc = Jsoup.parse(dataAsString);
+                Element masthead = doc.select("div.span10").get(1);
+                return masthead.toString();
+            } catch (IOException e) {
+                Log.d("error", e.getLocalizedMessage());
+                return "internal error. sorry.";
             }
-
-            // Pull content stream from response
-            HttpEntity entity = response.getEntity();
-            InputStream inputStream = entity.getContent();
-
-
-            ByteArrayOutputStream content = new ByteArrayOutputStream();
-
-            // Read response into a buffered stream
-            int readBytes = 0;
-            byte[] sBuffer = new byte[512];
-            while ((readBytes = inputStream.read(sBuffer)) != -1) {
-                content.write(sBuffer, 0, readBytes);
-            }
-
-            String dataAsString = new String(content.toByteArray());
-
-            Document doc = Jsoup.parse(dataAsString);
-            Element masthead = doc.select("div.span10").get(1);
-            return masthead.toString();
-        } catch (IOException e) {
-            Log.d("error", e.getLocalizedMessage());
-            return "internal error. sorry.";
         }
     }
-
 }
