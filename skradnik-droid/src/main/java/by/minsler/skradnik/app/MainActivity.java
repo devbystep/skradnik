4cf581996090c6d31020dfe2e6fdc34bfbac40c9
Dzmitry Misiuk <minsler.by@gmail.com>
2014-08-21 20:03:06 +0300

    Remove menu settings. Set default theme.

--- skradnik-droid/src/main/java/by/minsler/skradnik/app/MainActivity.java
+++ skradnik-droid/src/main/java/by/minsler/skradnik/app/MainActivity.java
@@ -1,12 +1,11 @@
 package by.minsler.skradnik.app;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.text.Html;
 import android.text.method.ScrollingMovementMethod;
-import android.view.Menu;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
@@ -28,7 +27,7 @@ import by.minsler.skradnik.entity.Translation;
 
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 
 
@@ -106,23 +105,6 @@ public class MainActivity extends ActionBarActivity {
         wordButton = (Button) findViewById(by.minsler.skradnik.app.R.id.button_translate);
         wordView.setMovementMethod(new ScrollingMovementMethod());
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(by.minsler.skradnik.app.R.menu.main, menu);
-        return true;
-    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
 
     public void translate_click(View v) {
         String wordFromEdit = wordEdit.getText().toString();
